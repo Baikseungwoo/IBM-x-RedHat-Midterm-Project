351387d4c48bd938f3ce86b6d3246f2117222821
Baikseungwoo <swbaik01@naver.com>
2026-04-18 14:52:11 +0900

    fix

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -4,15 +4,15 @@ from sqlalchemy import create_engine
 from app.core.settings import settings
 from sqlalchemy.orm import sessionmaker
 
-# 비동기 db연결 생성하는 함수
+# 비동기 db연결 생성
 async_engine=create_async_engine(settings.db_url, echo=False)
 
-# 비동기 엔진과 연결된 세션 사용하려고
+# 비동기 엔진과 연결된 세션 사용
 AsyncSessionLocal= sessionmaker(
     autocommit=False, autoflush=False, bind=async_engine, class_=AsyncSession
 )
 
-# 동기 db연결 생성하는 함수 (동기적으로 db와 연결한다))
+# 동기 db연결 생성
 sync_engine=create_engine(settings.sync_db_url, pool_pre_ping=True)
 
 # 기본 클래스 설정(Base)
