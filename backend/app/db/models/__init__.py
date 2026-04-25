43ba4153c1b633439e17416dc79a9d95d8c8dad2
Baikseungwoo <swbaik01@naver.com>
2026-04-25 17:20:58 +0900

    fix the name error in scheme init

--- backend/app/db/models/__init__.py
+++ backend/app/db/models/__init__.py
@@ -5,4 +5,4 @@ from app.db.models.review import Review
 from app.db.models.like import Like
 from app.db.models.bookmark import Bookmark
 from app.db.models.course import Course
-from app.db.models.course_detail import CourseDetail
+from app.db.models.course_detail import CourseItem
